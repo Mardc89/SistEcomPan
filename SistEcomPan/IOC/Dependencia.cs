9db6c537023f3eb79fdf362325a493d29a4ef8de
Mardc89 <mardc0602@gmail.com>
2023-10-31 14:05:19 -0500

    Configurando la capa IOC

---
+++ SistEcomPan/IOC/Dependencia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencias(this IServiceCollection services,IConfiguration configuration)
+        {
+
+        }
+    }
+}
