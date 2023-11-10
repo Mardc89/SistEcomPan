d688ead31efa4cbba9627451a9ba92120983a12d
Mardc89 <mardc0602@gmail.com>
2023-11-10 13:49:07 -0500

    Creando IUsuarioRepository y UsuarioController

---
+++ SistEcomPan/SistEcomPan.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Datos.Interfaces;
+using Entidades;
+
+namespace SistEcomPan.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IGenericRepository<Usuarios> _usuarios;
+
+        public UsuarioController(IGenericRepository<Usuarios> usuarios)
+        {
+            _usuarios = usuarios;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Crear([FromForm]IFormFile foto, [FromForm] string modelo)
+        {
+            return View();
+        }
+    }
+}
