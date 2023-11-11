24bdd17dea6f227b7311b3dcf23287941e7061c4
Mardc89 <mardc0602@gmail.com>
2023-11-11 13:34:08 -0500

    Agregando ProductoController y ClienteController

---
+++ SistEcomPan/SistEcomPan.Web/Controllers/ProductoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistEcomPan.Web.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
