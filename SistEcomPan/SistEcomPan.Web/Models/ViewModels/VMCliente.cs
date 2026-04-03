c722aabd94d580b4a14937c29c54c1f4a441bee5
Mardc89 <mardc0602@gmail.com>
2026-04-03 13:52:30 -0500

    Actualizando el Sistema

--- SistEcomPan/SistEcomPan.Web/Models/ViewModels/VMCliente.cs
+++ SistEcomPan/SistEcomPan.Web/Models/ViewModels/VMCliente.cs
@@ -1,22 +1,47 @@
 ﻿using Entidades;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistEcomPan.Web.Models.ViewModels
 {
     public class VMCliente
     {
-        public int IdCliente { get; set; }
-        public string? TipoCliente { get; set; }
-        public string? Dni { get; set; }
-        public string? Nombres { get; set; }
-        public string? Apellidos { get; set; }
-        public string? NombreCompleto { get; set; }
-        public string? Correo { get; set; }
-        public string? Direccion { get; set; }
-        public string? Telefono { get; set; }
+
+        [Required(ErrorMessage = "El DNI es obligatorio")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "El DNI debe tener 8 dígitos")]
+        public string Dni { get; set; }
+
+        [Required(ErrorMessage = "Los nombres son obligatorios")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "Los apellidos son obligatorios")]
+        public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [Phone(ErrorMessage = "Teléfono no válido")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Seleccione tipo de cliente")]
+        public string TipoCliente { get; set; }
+
+        [Required(ErrorMessage = "Seleccione un distrito")]
         public int IdDistrito { get; set; }
+
+        [Required(ErrorMessage = "La dirección es obligatoria")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo no válido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        public string NombreUsuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(6, ErrorMessage = "Mínimo 6 caracteres")]
+        public string Clave { get; set; }
+        public int IdCliente { get; set; }
+        public string? NombreCompleto { get; set; }
         public string? NombreDistrito { get; set; }
-        public string?NombreUsuario { get; set; }
-        public string? Clave { get; set; }
         public int? Estado { get; set; }
         public string? UrlFoto { get; set; }
         public string? NombreFoto { get; set; }
